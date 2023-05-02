810810d6d91046af8ebbde7d12515d6a04fec9f2
Merlin Mon Mathew <merlinmonmathew@gmail.com>
2023-05-02 23:53:28 +0530

    22nd submission

--- src/main/java/com/driver/services/impl/ReservationServiceImpl.java
+++ src/main/java/com/driver/services/impl/ReservationServiceImpl.java
@@ -38,12 +38,14 @@ public class ReservationServiceImpl implements ReservationService {
         int minPrice = Integer.MAX_VALUE;
         Spot minPricedSpot = null;
         for(Spot spot: spotList){
-            if((numberOfWheels>4 && spot.getSpotType()==SpotType.OTHERS) || (numberOfWheels>2 && numberOfWheels<=4 && spot.getSpotType()==SpotType.FOUR_WHEELER)
-            || (numberOfWheels<=2 && spot.getSpotType()==SpotType.TWO_WHEELER)){
-                int price = spot.getPricePerHour()*timeInHours;
-                if(price<minPrice){
-                    minPrice = price;
-                    minPricedSpot = spot;
+            if(spot.getOccupied()==false){
+                if((numberOfWheels>4 && spot.getSpotType()==SpotType.OTHERS) || (numberOfWheels>2 && numberOfWheels<=4 && spot.getSpotType()==SpotType.FOUR_WHEELER)
+                        || (numberOfWheels<=2 && spot.getSpotType()==SpotType.TWO_WHEELER)){
+                    int price = spot.getPricePerHour()*timeInHours;
+                    if(price<minPrice){
+                        minPrice = price;
+                        minPricedSpot = spot;
+                    }
                 }
             }
         }
